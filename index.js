c04e09d785e9e0ced4135d08e4d06364fada43a3
cptsoap6969 <swagbish4@gmail.com>
2017-07-23 17:33:42 +0100

    Update index.js

--- index.js
+++ index.js
@@ -85,46 +85,67 @@ var queue = [];function skip_song(message){
 
 }
 
-function playMusic(id, message) {
- voiceChannel = message.member.voiceChannel;
+function playMusic(id, message){
+    voiceChannel = message.member.voiceChannel;    voiceChannel.join().then(function (connection){
+        stream = ytdl("https://www.youtube.com/watch?v=" + id, {
+            filter: 'audioonly'
 
+        });
 
-  voiceChannel.join().then(function(connection) {
-      stream = ytdl("https://youtube.com/watch?v=" + id, {
-        filter: 'audioonly'
-      });
-      skipReq = 0;
-      skippers = [];
+        skipReq = 0;
+        skippers= [];        dispatcher = connection.playStream(stream);
+        dispatcher.on('end', function() {
+            skipReq = 0;
+            skippers = [];
+            var queue = [];
+            queue.shift();
+            if(queue.length == 0) {
+                queue = [];
+                isPlaying = false;
+            } else {
+                playMusic(queue[0], message);
 
-      dispatcher = connection.playStream(stream);
-  });
+            }
+
+        });
+
+    });
+
 }
 
 function getID(str, cb) {
-  if (isYoutube(str)) {
-    cb(getYouTubeID(str));
-  } else {
-    search_video(str, function(id) {
-      cb(id);
-    });
-  }
+    if(isYoutube(str)){
+        cb(getYouTubeID(str));
+    } else {
+        search_video(str, function(id) {
+            cb(id);
+
+        });
+
+    }
+
 }
 
 function add_to_queue(strID) {
-  if (isYoutube(strID, message)) {
-    queue.push(getYouTubeID(strID));
-  } else {
-    queue.push(strID);
-  }
+    if(isYoutube(strID)) {
+        queue.push(getYouTubeID(strID));
+    } else {
+        queue.push(strID);
+
+    }
+
 }
 
 function search_video(query, callback) {
-  request('https://www.googleapis.com/youtube/v3/search?part=id&type=video&q=' + encodeURIComponent(query) + '&key=' + yt_api_key, function(error, response, body) {
-    var json = JSON.parse(body);
-    callback(json.items[0].id.videoId);
-  });
+    request("https://www.googleapis.com/youtube/v3/search?part=id&type=video&q=" + encodeURIComponent(query) + "&key=" + yt_api_key, function(error, response, body) {
+        var json = JSON.parse(body);
+        callback(json.items[0].id.videoID);
+
+    });
+
 }
 
 function isYoutube(str) {
-  return str.toLowerCase().indexOf('youtube.com') > -1;
-}
+    return str.toLowerCase().indexOf("youtube.com") > -1;}
+
+bot.login(discord_token);﻿
